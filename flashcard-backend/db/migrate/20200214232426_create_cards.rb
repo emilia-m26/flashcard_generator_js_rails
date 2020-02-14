17a3d9500c582cabf3913f56cc026b5f0e0cc08e
Emilia <emilia.mena26@gmail.com>
2020-02-14 18:26:04 -0500

    change to card migrationt to add deck_id

--- flashcard-backend/db/migrate/20200214232426_create_cards.rb
+++ flashcard-backend/db/migrate/20200214232426_create_cards.rb
@@ -3,6 +3,7 @@ class CreateCards < ActiveRecord::Migration[6.0]
     create_table :cards do |t|
       t.string :question
       t.string :answer
+      t,integer :deck_id
 
       t.timestamps
     end
