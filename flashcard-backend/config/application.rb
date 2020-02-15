09849f1e5fffa263f8c5099feab0a674df558e54
Emilia <emilia.mena26@gmail.com>
2020-02-15 16:55:20 -0500

    fixed app.rb

--- flashcard-backend/config/application.rb
+++ flashcard-backend/config/application.rb
@@ -25,7 +25,7 @@ module FlashcardBackend
     config.middleware.insert_before 0, Rack::Cors do 
       allow do 
         origins '*'
-          resource '*', headers: :any, methods[:get, :post]
+          resource '*', headers: :any, methods: [:get, :post]
       end
     end 
 
