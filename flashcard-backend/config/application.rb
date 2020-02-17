d5136a92c627ec58f6b0f8c14ff2890d00c52b04
Emilia <emilia.mena26@gmail.com>
2020-02-17 09:38:18 -0500

    changes to config/app.rb to prepare to work on edit/update/delete

--- flashcard-backend/config/application.rb
+++ flashcard-backend/config/application.rb
@@ -25,7 +25,7 @@ module FlashcardBackend
     config.middleware.insert_before 0, Rack::Cors do 
       allow do 
         origins '*'
-          resource '*', headers: :any, methods: [:get, :post]
+          resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :options]
       end
     end 
 
