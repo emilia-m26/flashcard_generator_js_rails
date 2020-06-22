3cd7c0f424adc71e6dafba14614443e5917f9b90
Emilia <emilia.mena26@gmail.com>
2020-06-22 14:51:56 -0400

    had to clone, testing push

--- flashcard-backend/app/controllers/cards_controller.rb
+++ flashcard-backend/app/controllers/cards_controller.rb
@@ -22,6 +22,7 @@ class CardsController < ApplicationController
     @card = Card.new(card_params)
 
     if @card.save
+      
       render json: @card, status: :created, location: @card
     else
       render json: @card.errors, status: :unprocessable_entity
