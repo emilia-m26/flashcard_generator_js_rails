7963ec70f0444721f3e974563bd95c417426007e
Emilia <emilia.mena26@gmail.com>
2020-10-01 18:14:50 -0400

    changed schema

--- flashcard-backend/db/migrate/20200214232426_create_cards.rb
+++ flashcard-backend/db/migrate/20200214232426_create_cards.rb
@@ -1,9 +1,10 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
-      t.string :question
-      t.string :answer
+      t.string :card_front
+      t.string :card_back
       t.integer :deck_id
+      t.string :category
 
       t.timestamps
     end
--- flashcard-backend/db/schema.rb
+++ flashcard-backend/db/schema.rb
@@ -13,9 +13,10 @@
 ActiveRecord::Schema.define(version: 2020_02_14_232426) do
 
   create_table "cards", force: :cascade do |t|
-    t.string "question"
-    t.string "answer"
+    t.string "card_front"
+    t.string "card_back"
     t.integer "deck_id"
+    t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
